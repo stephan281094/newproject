9c6f1675cb4d27d209e8d7983e727ffb36df1208
anyascode <kieuanh997@gmail.com>
2023-12-24 19:02:37 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <post-header />
+    <post-footer />
+  </div>
+</template>
+<script>
+import PostHeader from "./components/PostHeader.vue";
+import PostFooter from "./components/PostFooter.vue";
+
+export default {
+  components: { PostHeader, PostFooter },
+  data() {
+    return {};
+  },
+};
+</script>
+<style>
+* {
+  background-color: #191919;
+  text-decoration: none;
+}
+</style>
---
+++ src/components/PostHeader.vue
@@ -0,0 +1,42 @@
+<template>
+  <header>
+    <el-row align-center>
+      <el-col :span="16"
+        ><div class="grid-content" />
+        <a href="#"
+          ><img src="../assets/images/IMAGE-2.svg" class="logoimg"
+        /></a>
+      </el-col>
+
+      <el-col :span="2"
+        ><div class="grid-content" />
+        <a href="#"><p class="navlink">Home</p></a></el-col
+      >
+      <el-col :span="2"
+        ><div class="grid-content" />
+        <a href="#"><p class="navlink">Episodes</p></a></el-col
+      >
+      <el-col :span="2"
+        ><div class="grid-content" />
+        <a href="#"><p class="navlink">About</p></a></el-col
+      >
+      <el-col :span="2"
+        ><div class="grid-content" />
+        <a href="#"><p class="navlink">Contact</p></a></el-col
+      >
+    </el-row>
+  </header>
+</template>
+<script></script>
+<style scoped>
+.logoimg {
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  margin-left: 1.5rem;
+}
+.navlink {
+  color: #ffffff;
+  font: 18px sans-serif;
+  text-align: center;
+}
+</style>
